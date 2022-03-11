db956169f75582343438a25820bbbc4c19ebe2a7
Valso <warut92@gmail.com>
2022-03-11 15:18:36 +0700

    Update date_th.js

--- portfolio/date_th.js
+++ portfolio/date_th.js
@@ -1,5 +1,5 @@
 var mydate=new Date()
-var year=mydate.getYear()+543
+var year=mydate.getYear()
 if (year < 1000)
     year+=1900
     var day=mydate.getDay()
@@ -8,4 +8,4 @@ if (year < 1000)
 if (daym<10)
 daym="0"+daym
 var montharray=new Array("มกราคม","กุมภาพันธ์","มีนาคม","เมษายน","พฤษภาคม","กรกฎาคม","มิถุนายน","สิงหาคม","กันยายน","ตุลาคม","พฤศจิกายน","ธันวาคม")
-document.write("<div style='padding: 10px;border: 1px #ccc solid;width: fit-content;border-radius: 7px;background-color: #fff;'>วันนี้วันที่  "+daym+" "+montharray[month]+" พ.ศ. "+year+"</div>")
+document.write("<div style='padding: 10px;border: 1px #ccc solid;width: fit-content;border-radius: 7px;background-color: #fff;'>วันนี้วันที่  "+daym+" "+montharray[month]+" ค.ศ. "+year+"</div>")
