d3a23d819ac32fc69cd503e715157aa9d0e26053
Valso <warut92@gmail.com>
2022-03-11 16:16:53 +0700

    update

--- portfolio/date_th.js
+++ portfolio/date_th.js
@@ -1,11 +1,12 @@
 var mydate=new Date()
-var year=mydate.getYear()
-if (year < 1000)
-    year+=1900
-    var day=mydate.getDay()
-    var month=mydate.getMonth()
-    var daym=mydate.getDate()
-if (daym<10)
-daym="0"+daym
-var montharray=new Array("มกราคม","กุมภาพันธ์","มีนาคม","เมษายน","พฤษภาคม","กรกฎาคม","มิถุนายน","สิงหาคม","กันยายน","ตุลาคม","พฤศจิกายน","ธันวาคม")
-document.write("<div style='padding: 10px;border: 1px #ccc solid;width: fit-content;border-radius: 7px;background-color: #fff;'>วันนี้วันที่  "+daym+" "+montharray[month]+" ค.ศ. "+year+"</div>")
+var year=mydate.getYear() + 543
+    if (year < 1000)
+        year+=1900
+        var day=mydate.getDay()
+        var month=mydate.getMonth()
+        var daym=mydate.getDate()
+            if (daym<10)
+                daym="0"+daym
+        var montharray=new Array("มกราคม","กุมภาพันธ์","มีนาคม","เมษายน","พฤษภาคม","กรกฎาคม","มิถุนายน","สิงหาคม","กันยายน","ตุลาคม","พฤศจิกายน","ธันวาคม")
+        
+        document.write("<div style='padding: 10px;border: 1px #ccc solid;width: fit-content;border-radius: 7px;background-color: #fff;'>วันนี้วันที่  "+daym+" "+montharray[month]+" พ.ศ. "+year+"</div>")
